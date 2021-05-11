bac3094c4ff5fd920f8b234a57494c661ce0891a
Dylan Gomer <36208962+dg1229@users.noreply.github.com>
2021-05-10 23:06:59 -0400

    Firebase authentication

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,16 @@ import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import Search from './components/Search';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Signup from './components/User/Signup';
+import { Container } from 'react-bootstrap'
+import { AuthProvider } from './contexts/AuthContext';
+import { auth } from './firebase';
+import Dashboard from './components/User/Dashboard'
+import Login from './components/User/Login'
+import PrivateRoute from "./components/PrivateRoute"
+import ForgotPassword from "./components/User/ForgotPassword"
+import UpdateProfile from './components/User/UpdateProfile'
 
 function App() {
   const [word, setWord] = React.useState('software');
@@ -15,12 +24,26 @@ function App() {
   };
 
   return (
-    <Router>      
-      <Nav />
-      <Route path='/' exact render={(props) => (
-        <Search />
-      )} />
-    </Router>
+    <Container
+      className="d-flex align-items-center justify-content-center"
+      style={{ minHeight: "100vh" }}
+      >
+        <Nav fixed="top" />
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Router>
+            <AuthProvider>
+              <Switch>
+                <PrivateRoute exact path="/" component={Dashboard} />
+                <PrivateRoute path="/update-profile" component={UpdateProfile} />
+                <Route path="/search" component={Search} />
+                <Route path="/signup" component={Signup} />
+                <Route path="/login" component={Login} />
+                <Route path="/forgot-password" component={ForgotPassword} />
+              </Switch>
+            </AuthProvider>
+          </Router>
+        </div>
+      </Container>
   );
 }
 
---
+++ client/src/firebase.js
@@ -0,0 +1,15 @@
+import firebase from'firebase/app';
+import "firebase/auth";
+
+const app = firebase.initializeApp({
+    apiKey: "AIzaSyDnBLj2oHRqOg9mm0kwvYX2wfpN82M7LUw",
+    authDomain: "streamfinder-7a41e.firebaseapp.com",
+    databaseURL: "https://streamfinder-7a41e-default-rtdb.firebaseio.com/",
+    projectId: "streamfinder-7a41e",
+    storageBucket: "streamfinder-7a41e.appspot.com",
+    messagingSenderId: "860722722237",
+    appId: "1:860722722237:web:57cd4c756f73952c500ed4"
+})
+
+export const auth = app.auth();
+export default app;
